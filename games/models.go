eb03b1b9c17b0eab3578cd5436b3a3455e6222ba
Mario Jiménez <mario.mj16@gmail.com>
2021-05-12 22:42:19 -0600

    Modified: game data Added: game list

--- games/models.go
+++ games/models.go
@@ -1,9 +1,17 @@
 package games
 
-type Game struct {
-	ID          string   `json:"id"`
-	Name        string   `json:"name"`
-	ScoreURL    string   `json:"score_url"`
-	DurationURL string   `json:"duration_url"`
-	PricesURLs  []string `json:"prices_urls"`
-}
+type (
+	Game struct {
+		ID          string  `json:"id"`
+		Name        string  `json:"name"`
+		BasePrice   float32 `json:"base_price"`
+		ScoreURL    string  `json:"score_url"`
+		DurationURL string  `json:"duration_url"`
+		PricesURLs  []price `json:"prices_urls"`
+	}
+
+	price struct {
+		From string `json:"from"`
+		URL  string `json:"url"`
+	}
+)
